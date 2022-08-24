61ea48157540a6e75c0bf6e557314e2267d52967
AleksIvanVel <89878433+AleksIvanVel@users.noreply.github.com>
2022-08-24 16:55:22 -0500

    Add files via upload

--- Arreglo.java
+++ Arreglo.java
@@ -3,9 +3,9 @@ package com.mycompany.clasearreglo;
 import java.util.Scanner;
 
 public class Arreglo {
-    int indice=0;
-    int valor=0;
-    int tamanioArray=0;
+    int indice;
+    int valor;
+    int tamanioArray;
 
 
     public Arreglo(){
@@ -21,18 +21,15 @@ public class Arreglo {
     }
 
     public static void arreglo(int tamanioArray){
-       Scanner entrada= new Scanner(System.in);
-       System.out.println("Ingrese el tamanio del arreglo: ");
-       tamanioArray = entrada.nextInt();
        int []arreglo = new int[tamanioArray];
        System.out.println("Arreglo creado");
     }
 
-    public static String getLongitud(){
-       return "La longitued del arreglo es: " + arreglo.length;
+    public static int obtenerLongitud(){
+       return "El tamanio del arreglo es: " + arreglo.length;
     }
 
-    public  void setElemento(int indice, int valor){
+    public void agregarElemento(int indice, int valor){
          Scanner entrada= new Scanner(System.in);
          System.out.println("Ingrese el nuevo elemento: ");
          valor = entrada.nextInt();
@@ -48,26 +45,24 @@ public class Arreglo {
          Scanner entrada= new Scanner(System.in);
          System.out.println("Ingrese un indice entre 0 y" + tamanioArray);
          indice = entrada.nextInt();
-       return arreglo[indice];
+       return "El elemento seleccionado des: " + arreglo[indice];
     }
 
     public  void limpiar(int valor){
          Scanner entrada= new Scanner(System.in);
          System.out.println("Ingrese el valor con el desea limpiar el arreglo: ");
          valor = entrada.nextInt();
-         for(int i=0; i==tamanioArray ; i++){
+         for(int i=0; i==tamanioArray-1; i++){
              arreglo[i] = valor;
+             arreglo[i] = arreglo[i+1];
          }
          System.out.println("Limpieza Realizada ");
         
-        }
+    }
 
-public String toString(){
-  return "elemento 1" + arreglo[0] + "elemento 2" + arreglo[1] + "elemento 3" + arreglo[2] 
-          + "elemento 4" + arreglo[3]; 
-}
-
-  
-  
+    public String toString(){
+      return "elemento 1" + arreglo[0] + "elemento 2" + arreglo[1] + "elemento 3" + arreglo[2] 
+              + "elemento 4" + arreglo[3]; 
+    }
          
 }
